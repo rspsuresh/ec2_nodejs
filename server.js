83fb5b0e2d501f680032bdf7b4b807016ea7e87a
rspsuresh <rsprampaul14321@gmail.com>
2026-08-01 20:52:55 +0530

    gg

--- server.js
+++ server.js
@@ -22,11 +22,6 @@ app.use(express.json());
 // Routes
 app.use("/api/users", userRoutes);
 
-app.use("/api/products", (req, res) => {
-  return res.status(200).json({
-    message: 'This is new feature change, a new route for products'
-  })
-});
 
 app.use("/api/products", (req, res) => {
   return res.status(200).json({
